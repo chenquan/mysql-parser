84fc4345c90acea196515540fc684cbe7897614f
chenquan <chenquan.dev@gmail.com>
2022-10-23 17:52:03 +0800

    chore: reformat code

--- parser_test.go
+++ parser_test.go
@@ -1,12 +1,9 @@
 package parser
 
-//func TestParseTreeVisitor_VisitCreateDatabase(t *testing.T) {
-//
-//	inputStream := antlr.NewInputStream("CREATE TABLE IOT( A INT);CREATE TABLE IOT1( A INT);CREATE DATABASE `name`;")
-//	lexer := parser.NewMySqlLexer(inputStream)
-//	lexer.RemoveErrorListeners()
-//	tokens := antlr.NewCommonTokenStream(lexer, antlr.LexerDefaultTokenChannel)
-//	mysqlParser := parser.NewMySqlParser(tokens)
-//	visitor := &parseTreeVisitor{}
-//	mysqlParser.Root().Accept(visitor)
-//}
+import (
+	"testing"
+)
+
+func Test_parseTreeVisitor_VisitDmlStatement(t *testing.T) {
+
+}
--- sql_test.go
+++ sql_test.go
@@ -1,19 +1,1 @@
 package parser
-
-//func TestParser(t *testing.T) {
-//	t.Run("", func(t *testing.T) {
-//		Parser("CREATE TABLE IOT( A INT);CREATE TABLE IOT1( A INT);CREATE DATABASE TT;DROP DATABASE TT;")
-//
-//	})
-//
-//	t.Run("ALTER DATABASE", func(t *testing.T) {
-//		Parser("ALTER DATABASE A DEFAULT CHARACTER SET gb2312")
-//	})
-//
-//	t.Run("ALTER TABLE", func(t *testing.T) {
-//		t.Run("ADD COLUMN", func(t *testing.T) {
-//			//Parser("ALTER TABLE user ADD (age INT , class int ,user varchar(2) )")
-//			Parser("ALTER TABLE user ADD IF NOT EXISTS age INT ,Add IF NOT EXISTS class varchar(20);ALTER TABLE user ADD IF NOT EXISTS age INT ,Add IF NOT EXISTS class varchar(1001);")
-//		})
-//	})
-//}
