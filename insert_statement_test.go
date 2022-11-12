d4c98500dd97a683402a0802b8f9d3bf65733d0b
chenquan <chenquan.dev@gmail.com>
2022-11-12 20:30:41 +0800

    feat: support insertStatement

---
+++ insert_statement_test.go
@@ -0,0 +1,132 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_parseTreeVisitor_VisitInsertStatement(t *testing.T) {
+	t.Run("1", func(t *testing.T) {
+		mySqlParser, visitor := createMySqlParser("INSERT INTO A VALUES (1,2),(2,3)")
+		result := mySqlParser.InsertStatement().Accept(visitor)
+		assert.EqualValues(t,
+			InsertStatementIntoValue{
+				Priority: "",
+				Ignore:   false,
+				TableName: TableName{
+					Uid:   "A",
+					DotId: "",
+				},
+				Columns: nil,
+				InsertStatementValue: InsertStatementValuedExpressionsWithDefaults{ExpressionsWithDefaultsList: []ExpressionsWithDefaults{
+					{
+						ExpressionOrDefaults: []ExpressionOrDefault{
+							{
+								Default:    false,
+								Expression: ExpressionAtomPredicate{ExpressionAtom: ConstantExpressionAtom{Constant: ConstantDecimal{Val: 1}}},
+							},
+							{
+								Default:    false,
+								Expression: ExpressionAtomPredicate{ExpressionAtom: ConstantExpressionAtom{Constant: ConstantDecimal{Val: 2}}},
+							},
+						},
+					},
+					{
+						ExpressionOrDefaults: []ExpressionOrDefault{
+							{
+								Default:    false,
+								Expression: ExpressionAtomPredicate{ExpressionAtom: ConstantExpressionAtom{Constant: ConstantDecimal{Val: 2}}},
+							},
+							{
+								Default:    false,
+								Expression: ExpressionAtomPredicate{ExpressionAtom: ConstantExpressionAtom{Constant: ConstantDecimal{Val: 3}}},
+							},
+						},
+					},
+				}},
+			},
+			result)
+	})
+
+	t.Run("2", func(t *testing.T) {
+		mySqlParser, visitor := createMySqlParser("INSERT INTO A(a,b) VALUES (1,2),(2,3)")
+		result := mySqlParser.InsertStatement().Accept(visitor)
+		assert.EqualValues(t,
+			InsertStatementIntoValue{
+				Priority: "",
+				Ignore:   false,
+				TableName: TableName{
+					Uid:   "A",
+					DotId: "",
+				},
+				Columns: []string{"a", "b"},
+				InsertStatementValue: InsertStatementValuedExpressionsWithDefaults{ExpressionsWithDefaultsList: []ExpressionsWithDefaults{
+					{
+						ExpressionOrDefaults: []ExpressionOrDefault{
+							{
+								Default:    false,
+								Expression: ExpressionAtomPredicate{ExpressionAtom: ConstantExpressionAtom{Constant: ConstantDecimal{Val: 1}}},
+							},
+							{
+								Default:    false,
+								Expression: ExpressionAtomPredicate{ExpressionAtom: ConstantExpressionAtom{Constant: ConstantDecimal{Val: 2}}},
+							},
+						},
+					},
+					{
+						ExpressionOrDefaults: []ExpressionOrDefault{
+							{
+								Default:    false,
+								Expression: ExpressionAtomPredicate{ExpressionAtom: ConstantExpressionAtom{Constant: ConstantDecimal{Val: 2}}},
+							},
+							{
+								Default:    false,
+								Expression: ExpressionAtomPredicate{ExpressionAtom: ConstantExpressionAtom{Constant: ConstantDecimal{Val: 3}}},
+							},
+						},
+					},
+				}},
+			},
+			result)
+	})
+
+	t.Run("3", func(t *testing.T) {
+		mySqlParser, visitor := createMySqlParser("INSERT DELAYED Ignore INTO A SET a=1,b=2")
+		result := mySqlParser.InsertStatement().Accept(visitor)
+		assert.EqualValues(t,
+			InsertStatementSetValue{
+				Priority: "DELAYED",
+				Ignore:   true,
+				TableName: TableName{
+					Uid:   "A",
+					DotId: "",
+				},
+				UpdatedElements: []UpdatedElement{
+					{
+						FullColumnName: FullColumnName{
+							Uid:       "a",
+							DottedIds: nil,
+						},
+						Value: ExpressionOrDefault{
+							Default:    false,
+							Expression: ExpressionAtomPredicate{ExpressionAtom: ConstantExpressionAtom{Constant: ConstantDecimal{Val: 1}}},
+						},
+					},
+
+					{
+						FullColumnName: FullColumnName{
+							Uid:       "b",
+							DottedIds: nil,
+						},
+						Value: ExpressionOrDefault{
+							Default:    false,
+							Expression: ExpressionAtomPredicate{ExpressionAtom: ConstantExpressionAtom{Constant: ConstantDecimal{Val: 2}}},
+						},
+					},
+				},
+			},
+			result)
+	})
+
+}
