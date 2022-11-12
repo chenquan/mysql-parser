394f8948ffa50c18b6f12c2ca5daaa4529d0b0a6
chen quan <chenquan.dev@gmail.com>
2022-11-12 20:02:55 +0800

    feat: support insertStatementValue (#31)
    * feat: support insertStatementValue
    
    * feat: support insertStatementValue

--- expressions_with_defaults_test.go
+++ expressions_with_defaults_test.go
@@ -10,7 +10,7 @@ func Test_parseTreeVisitor_VisitExpressionsWithDefaults(t *testing.T) {
 	t.Run("1", func(t *testing.T) {
 		mySqlParser, visitor := createMySqlParser("1!=1 AND a!=2")
 		result := mySqlParser.ExpressionsWithDefaults().Accept(visitor)
-		assert.EqualValues(t, []ExpressionOrDefault{
+		assert.EqualValues(t, ExpressionsWithDefaults{ExpressionOrDefaults: []ExpressionOrDefault{
 			{Expression: LogicalExpression{
 				LeftExpression: BinaryComparisonPredicate{
 					LeftPredicate:      ExpressionAtomPredicate{ExpressionAtom: ConstantExpressionAtom{Constant: ConstantDecimal{Val: 1}}},
@@ -26,7 +26,7 @@ func Test_parseTreeVisitor_VisitExpressionsWithDefaults(t *testing.T) {
 					RightPredicate:     ExpressionAtomPredicate{ExpressionAtom: ConstantExpressionAtom{Constant: ConstantDecimal{Val: 2}}},
 				},
 			}},
-		}, result)
+		}}, result)
 	})
 
 }
