bc15effe331e278822c0cf9d9057529c674533ec
chen quan <chenquan.dev@gmail.com>
2022-11-12 16:43:12 +0800

    feat: support alterDatabase (#27)

---
+++ alter_database.go
@@ -0,0 +1,52 @@
+package parser
+
+import (
+	"github.com/chenquan/mysql-parser/internal/parser"
+)
+
+var (
+	_ AlterDatabase = (*AlterSimpleDatabase)(nil)
+	_ AlterDatabase = (*AlterUpgradeName)(nil)
+)
+
+type (
+	AlterDatabase interface {
+		IsAlterDatabase()
+	}
+	AlterSimpleDatabase struct {
+		DatabaseName          string
+		CreateDatabaseOptions []CreateDatabaseOption
+	}
+	AlterUpgradeName struct {
+		DatabaseName string
+	}
+)
+
+func (a AlterUpgradeName) IsAlterDatabase() {
+}
+
+func (a AlterSimpleDatabase) IsAlterDatabase() {
+}
+
+func (v *parseTreeVisitor) VisitAlterSimpleDatabase(ctx *parser.AlterSimpleDatabaseContext) interface{} {
+	var databaseName string
+	uidContext := ctx.Uid()
+	if uidContext != nil {
+		databaseName = uidContext.GetText()
+	}
+
+	allDatabaseOption := ctx.AllCreateDatabaseOption()
+	databaseOptions := make([]CreateDatabaseOption, 0, len(allDatabaseOption))
+	for _, databaseOptionContext := range allDatabaseOption {
+		databaseOptions = append(databaseOptions, databaseOptionContext.Accept(v).(CreateDatabaseOption))
+	}
+
+	return AlterSimpleDatabase{
+		DatabaseName:          databaseName,
+		CreateDatabaseOptions: databaseOptions,
+	}
+}
+
+func (v *parseTreeVisitor) VisitAlterUpgradeName(ctx *parser.AlterUpgradeNameContext) interface{} {
+	return AlterUpgradeName{DatabaseName: ctx.Uid().GetText()}
+}
